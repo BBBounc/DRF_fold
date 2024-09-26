6f9da2e4f5ee2962e5ca9eadd49a38e03adce6f2
Vekyn <vekyn10@mail.ru>
2024-09-26 15:08:12 +0300

    update page information

--- fold/storage/views.py
+++ fold/storage/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
 
 def index(request):
   return render(request, 'index.html')  
@@ -6,4 +7,16 @@ def index(request):
 
 
 def information(request):
-  return render(request, 'information.html')  
+    products = Product.objects.all()
+    warehouses = Warehouse.objects.all()
+    type_organizations = TypeOrganization.objects.all()
+    organizations = Organization.objects.all()
+    supplies = Supply.objects.all()
+    context = {
+        'tovar': products,
+        'sklad': warehouses,
+        'TypeOrg': type_organizations,
+        'organization': organizations,
+        'postavka': supplies
+    }
+    return render(request, 'information.html', context)  
